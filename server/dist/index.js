144cd6700b13787987d65fb6cc3096caea1b66ae
midghaar <anton_molgan@hotmail.com>
2019-02-02 13:03:09 +0100

    WIP

--- server/dist/index.js
+++ server/dist/index.js
@@ -4,13 +4,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
 const app = express_1.default();
 const port = 8080; // default port to listen
+// Configure Express to use EJS
+app.set("views", path_1.default.join(__dirname, "views"));
+app.set("view engine", "ejs");
 // define a route handler for the default home page
 app.get("/", (req, res) => {
-    res.send("Hello world!");
+    // render the index template
+    res.render("index");
 });
-// start the Express server
+// start the express server
 app.listen(port, () => {
     // tslint:disable-next-line:no-console
     console.log(`server started at http://localhost:${port}`);
